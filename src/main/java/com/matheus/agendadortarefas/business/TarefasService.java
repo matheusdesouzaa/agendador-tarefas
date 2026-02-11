87b807a7c26ed563cb9d28cfbf34283dab85f3d0
Matheus de Souza <theu205@gmail.com>
2026-02-11 13:48:01 -0300

    adicionados metodos get por email e por periodo

--- src/main/java/com/matheus/agendadortarefas/business/TarefasService.java
+++ src/main/java/com/matheus/agendadortarefas/business/TarefasService.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -26,5 +27,16 @@ public class TarefasService {
         dto.setEmailUsuario(email);
         TarefasEntity entity = tarefaConverter.paraTarefaEntity(dto);
         return tarefaConverter.paraTarefaDTO(tarefasRepository.save(entity));
+    }
+
+    public List<TarefasDTO> buscarTarefasPorPeriodo(LocalDateTime dataInicial,LocalDateTime dataFinal){
+        return tarefaConverter.paraListaTarefaDTO(
+                tarefasRepository.findByDataEventoBetween(dataInicial,dataFinal));
+    }
+
+    public List<TarefasDTO> buscarTarefasPorEmail(String token){
+        String email = jwtUtil.extractUsername(token.substring(7));
+        List<TarefasEntity> listaTarefas = tarefasRepository.findByEmailUsuario(email);
+        return tarefaConverter.paraListaTarefaDTO(listaTarefas);
     }
 }
