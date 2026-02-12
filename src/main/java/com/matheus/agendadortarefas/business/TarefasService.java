4bb4bdfc6996f8171e510e6abbe9d05a2c1d6118
Matheus de Souza <theu205@gmail.com>
2026-02-12 13:38:13 -0300

    adicionados metodos de delete,patch de status e put de tarefas

--- src/main/java/com/matheus/agendadortarefas/business/TarefasService.java
+++ src/main/java/com/matheus/agendadortarefas/business/TarefasService.java
@@ -2,8 +2,10 @@ package com.matheus.agendadortarefas.business;
 
 import com.matheus.agendadortarefas.business.dto.TarefasDTO;
 import com.matheus.agendadortarefas.business.mapper.TarefaConverter;
+import com.matheus.agendadortarefas.business.mapper.TarefaUpdateConverter;
 import com.matheus.agendadortarefas.infrastructure.entity.TarefasEntity;
 import com.matheus.agendadortarefas.infrastructure.enums.StatusNotificacaoEnum;
+import com.matheus.agendadortarefas.infrastructure.exceptions.ResourceNotFoundException;
 import com.matheus.agendadortarefas.infrastructure.repository.TarefasRepository;
 import com.matheus.agendadortarefas.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
@@ -19,8 +21,9 @@ public class TarefasService {
     private final TarefasRepository tarefasRepository;
     private final TarefaConverter tarefaConverter;
     private final JwtUtil jwtUtil;
+    private final TarefaUpdateConverter tarefaUpdateConverter;
 
-    public TarefasDTO gravarTarefa(String token, TarefasDTO dto){
+    public TarefasDTO gravarTarefa(String token, TarefasDTO dto) {
         String email = jwtUtil.extractUsername(token.substring(7));
         dto.setDataCriacao(LocalDateTime.now());
         dto.setStatusNotificacaoEnum(StatusNotificacaoEnum.PENDENTE);
@@ -29,14 +32,45 @@ public class TarefasService {
         return tarefaConverter.paraTarefaDTO(tarefasRepository.save(entity));
     }
 
-    public List<TarefasDTO> buscarTarefasPorPeriodo(LocalDateTime dataInicial,LocalDateTime dataFinal){
+    public List<TarefasDTO> buscarTarefasPorPeriodo(LocalDateTime dataInicial, LocalDateTime dataFinal) {
         return tarefaConverter.paraListaTarefaDTO(
-                tarefasRepository.findByDataEventoBetween(dataInicial,dataFinal));
+                tarefasRepository.findByDataEventoBetween(dataInicial, dataFinal));
     }
 
-    public List<TarefasDTO> buscarTarefasPorEmail(String token){
+    public List<TarefasDTO> buscarTarefasPorEmail(String token) {
         String email = jwtUtil.extractUsername(token.substring(7));
         List<TarefasEntity> listaTarefas = tarefasRepository.findByEmailUsuario(email);
         return tarefaConverter.paraListaTarefaDTO(listaTarefas);
+    }
+
+    public void deletaPorId(String id) {
+        try {
+            tarefasRepository.deleteById(id);
+        } catch (ResourceNotFoundException e) {
+            throw new ResourceNotFoundException("erro ao deletar tarefa por id, id inexistente " +
+                    id, e.getCause());
+        }
+    }
+
+    public TarefasDTO alteraStatus(StatusNotificacaoEnum status, String id) {
+        try {
+            TarefasEntity entity = tarefasRepository.findById(id)
+                    .orElseThrow(() -> new RuntimeException("Tarefa não encontrada " + id));
+            entity.setStatusNotificacaoEnum(status);
+            return tarefaConverter.paraTarefaDTO(tarefasRepository.save(entity));
+        } catch (ResourceNotFoundException e) {
+            throw new ResourceNotFoundException("Erro ao alterar status da tarefa " + e.getCause());
+        }
+    }
+
+    public TarefasDTO updateTarefa(TarefasDTO dto,String id) {
+        try {
+            TarefasEntity entity = tarefasRepository.findById(id)
+                    .orElseThrow(() -> new RuntimeException("Tarefa não encontrada " + id));
+            tarefaUpdateConverter.updateTarefas(dto, entity);
+            return tarefaConverter.paraTarefaDTO(tarefasRepository.save(entity));
+        } catch (ResourceNotFoundException e) {
+            throw new ResourceNotFoundException("Erro ao alterar status da tarefa " + e.getCause());
+        }
     }
 }
--- src/main/java/com/matheus/agendadortarefas/controller/TarefasController.java
+++ src/main/java/com/matheus/agendadortarefas/controller/TarefasController.java
@@ -2,6 +2,7 @@ package com.matheus.agendadortarefas.controller;
 
 import com.matheus.agendadortarefas.business.TarefasService;
 import com.matheus.agendadortarefas.business.dto.TarefasDTO;
+import com.matheus.agendadortarefas.infrastructure.enums.StatusNotificacaoEnum;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
@@ -19,20 +20,37 @@ public class TarefasController {
 
     @PostMapping
     public ResponseEntity<TarefasDTO> gravarTarefa(@RequestBody TarefasDTO dto,
-                                                   @RequestHeader("Authorization") String token){
+                                                   @RequestHeader("Authorization") String token) {
         return ResponseEntity.ok(tarefasService.gravarTarefa(token, dto));
     }
 
     @GetMapping("/eventos")
     public ResponseEntity<List<TarefasDTO>> buscarListaDeTarefasPorPeriodo(
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)LocalDateTime dataInicial,
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)LocalDateTime dataFinal
-            ){
-        return ResponseEntity.ok(tarefasService.buscarTarefasPorPeriodo(dataInicial,dataFinal));
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dataInicial,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dataFinal
+    ) {
+        return ResponseEntity.ok(tarefasService.buscarTarefasPorPeriodo(dataInicial, dataFinal));
     }
 
     @GetMapping
-    public ResponseEntity<List<TarefasDTO>> buscarTarefaPorEmail(@RequestHeader("Authorization") String token){
+    public ResponseEntity<List<TarefasDTO>> buscarTarefaPorEmail(@RequestHeader("Authorization") String token) {
         return ResponseEntity.ok(tarefasService.buscarTarefasPorEmail(token));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletarPorId(@RequestParam("id") String id) {
+        tarefasService.deletaPorId(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping
+    public ResponseEntity<TarefasDTO> alterarStatusNotificacao(@RequestParam("status") StatusNotificacaoEnum status,
+                                                               @RequestParam("id") String id) {
+        return ResponseEntity.ok(tarefasService.alteraStatus(status, id));
+    }
+
+    @PutMapping
+    public ResponseEntity<TarefasDTO> updateDeTarefa(@RequestBody TarefasDTO dto, @RequestParam("id") String id) {
+        return ResponseEntity.ok(tarefasService.updateTarefa(dto, id));
     }
 }
